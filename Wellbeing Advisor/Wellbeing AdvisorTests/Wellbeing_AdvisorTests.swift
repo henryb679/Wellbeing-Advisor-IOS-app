63a84edbc0ff5543d1a3c6f3546a3ad427572287
Henry <henryb679@outlook.com>
2019-06-22 22:58:12 +1200

    #1 - Pushed all the raw xcode files to github

---
+++ Wellbeing Advisor/Wellbeing AdvisorTests/Wellbeing_AdvisorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wellbeing_AdvisorTests.swift
+//  Wellbeing AdvisorTests
+//
+//  Created by Henry Bai on 22/06/19.
+//  Copyright © 2019 Henry Bai. All rights reserved.
+//
+
+import XCTest
+@testable import Wellbeing_Advisor
+
+class Wellbeing_AdvisorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
