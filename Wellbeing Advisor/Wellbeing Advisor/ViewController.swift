e1d7af19ef752ec54205c1a401c631eeebe4855c
Henry <henryb679@outlook.com>
2019-06-28 21:51:09 +1200

    #3 - UI elements are now integrated into the code

--- Wellbeing Advisor/Wellbeing Advisor/ViewController.swift
+++ Wellbeing Advisor/Wellbeing Advisor/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var app_title: UILabel!
+    @IBOutlet weak var health_outlook: UITextField!
+    
+    
+    @IBOutlet weak var output_result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //handles UI text field by passing a valid text field delegate/
+        //the self refers to the view controller class
+        health_outlook.delegate = self
     }
 
+    @IBAction func submit_button(_ sender: UIButton) {
+        output_result.text = health_outlook.text
+        
+        //when submit button is pressed - the text box is cleared
+        health_outlook.text = ""
 
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        //hides the keyboard once the user has pressed the return button on the keyboard
+        health_outlook.resignFirstResponder()
+        
+        return true
+    }
+  
+    
 }
 
